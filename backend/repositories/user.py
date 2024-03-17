451fad97daaa04529f3a45ea0d73629ea33e183b
v.krylyshkin <v.krylyshkin@mail.kryptonite.ru>
2024-03-17 15:44:56 +0300

    add get all users and by id

--- backend/repositories/user.py
+++ backend/repositories/user.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.models.user import UserRole
 from backend.schemas.user import UserDto
@@ -7,11 +10,20 @@ from backend.models import User
 class UserRepository:
     async def create_user(self, db: AsyncSession, user_data: UserDto) -> User:
         new_user = User(
-            hashed_password = user_data.password + 'hash)))',
-            name = user_data.login,
-            user_role = UserRole.USER
+            hashed_password=user_data.password + 'hash)))',
+            name=user_data.login,
+            user_role=UserRole.USER
         )
         db.add(new_user)
         await db.commit()
         await db.refresh(new_user)
         return new_user
+
+    async def get_all_user(self, db: AsyncSession) -> Sequence[User]:
+        stmt = select(User).order_by(User.id)
+        result = await db.execute(stmt)
+        users = result.scalars().all()
+        return users
+
+    async def det_user_by_id(self, bd: AsyncSession, user_id: int) -> User | None:
+        return await bd.get(User, user_id)
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,4 +1,9 @@
+from typing import Sequence
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.models import User
 from backend.schemas.user import UserDto
 from backend.repositories.user import UserRepository
 
@@ -6,5 +11,18 @@ from backend.repositories.user import UserRepository
 async def create_user(
         db: AsyncSession,
         user_data: UserDto,
-        user_repository = UserRepository()) -> UserDto:
+        user_repository=UserRepository()) -> UserDto:
     return await user_repository.create_user(db=db, user_data=user_data)
+
+
+async def get_all_users(
+        db: AsyncSession,
+        user_repository=UserRepository()) -> Sequence[User]:
+    return await user_repository.get_all_user(db)
+
+
+async def get_user_by_id(
+        db: AsyncSession,
+        id: int,
+        user_repository=UserRepository()):
+    return await user_repository.det_user_by_id(db, id)
